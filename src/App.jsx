332affb1da025b3f5b836b5e34d53c28f6971dd2
Kang.__.Mingu <jkang4531@naver.com>
2022-09-03 13:27:30 +0900

    Portfolio: Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
-import Portfoilo from "./components/portfoilo/Portfoilo";
+import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Footer from "./components/footer/Footer";
 import Contact from "./components/contact/Contact";
@@ -17,7 +17,7 @@ export const App = () => {
       <About />
       <Experience />
       <Services />
-      <Portfoilo />
+      <Portfolio />
       <Testimonials />
       <Contact />
       <Footer />
--- src/components/portfoilo/Portfoilo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./portfoilo.css";
-
-const Portfoilo = () => {
-  return <section id="portfoilo">Portfoilo</section>;
-};
-
-export default Portfoilo;
